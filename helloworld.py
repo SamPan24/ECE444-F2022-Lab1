585493b392f1dfee271b62acbde1ec333725fa91
Sam Pan <SamPan@MacBook-Pro-61.local>
2022-09-19 17:49:57 -0400

    fixing conflict

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,8 @@
 print("Hello World");
 print("I have been at U of T for 4 years");
+
 print("C3 for activity 5")
 print("C4 for activity 5")
+
+print("C1 for Activity 5")
+print("C2 for Activity 5")
